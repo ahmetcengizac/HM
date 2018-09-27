363d104be5c6375c0017aab333dfb6f0fd2c7994
Ahmet Cengiz <ahmetcengiz@hotmail.com>
2018-09-27 15:04:08 +0100

    after finish insert- db oper

---
+++ data/dboperator.py
@@ -0,0 +1,25 @@
+from util.static import app
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:first@localhost/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy( app )
+ma = Marshmallow( app )
+
+def insert(self):
+    db.session.add( self )
+    db.session.commit()
+    return self.id
+
+
+def update(self):
+    db.session.add( self )
+    db.session.commit()
+
+
+def delete(self, **pid):
+    rec = self.query.get( pid )
+    db.session.delete( rec )
+    db.session.commit()
